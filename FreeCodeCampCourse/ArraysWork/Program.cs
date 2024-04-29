94b1e4e0a7a8e32664c3b64128be809adf218499
Sithembiso Gama <isitheh@gmail.com>
2024-04-29 08:22:33 +0200

    Commit the latest code to the repo.

--- FreeCodeCampCourse/ArraysWork/Program.cs
+++ FreeCodeCampCourse/ArraysWork/Program.cs
@@ -13,7 +13,8 @@ namespace ArraysWork
         {
             JustArrays();
             TriangleArrays();
-            ArraySort();
+            ArraySortReverse();
+            ArrayIndexOf();
             Console.ReadLine();
         }
 
@@ -51,7 +52,7 @@ namespace ArraysWork
             Console.WriteLine(sum == 180 ? "Valid Triangle" : "Invalid Tiangle");
         }
 
-        static void ArraySort()
+        static void ArraySortReverse()
         {
             int[] numbers = new int[]
             {
@@ -62,7 +63,129 @@ namespace ArraysWork
 
             foreach (int num in numbers)
             {
-                Console.Write($"{num}");
+                Console.Write($"{num} ");
+            }
+
+            Console.WriteLine();
+
+            Array.Reverse(numbers);
+
+            foreach (int num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+
+            Console.WriteLine();
+            //Clears the array to default values for the data type (0 for integers)
+            Array.Clear(numbers, 0, numbers.Length);
+
+            foreach (int num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+
+            Console.WriteLine();
+
+            string[] strings = new string[]
+            {
+                "Hello", "World", "of", "C#", "Programming", "Language"
+            };
+
+            foreach (string str in strings)
+            {
+                Console.Write($"{str} ");
+            }
+
+            Console.WriteLine();
+            //Sorts the array in alphabetical order
+            Array.Sort(strings);
+
+            foreach (string str in strings)
+            {
+                Console.Write($"{str} ");
+            }
+
+            Console.WriteLine();
+
+            //Reverses the array
+            Array.Reverse(strings);
+
+            foreach (string str in strings)
+            {
+                Console.Write($"{str} ");
+            }
+
+            Console.WriteLine();
+
+            //Clears the array to default values for the data type (null for strings)
+            Array.Clear(strings, 0, strings.Length);
+            foreach (string str in strings)
+            {
+                Console.Write($"{str} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        static void ArrayIndexOf()
+        {
+            int[] numbers = new int[]
+            {
+                 8, 7, 3, 1, 5, 9, 2, 0, 4, 6
+            };
+
+            Console.WriteLine("Position: ");
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{i + 1} ");
+            }
+
+            Console.WriteLine();
+
+            foreach (int num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Enter a number to search: ");
+            int search = Convert.ToInt32(Console.ReadLine());
+            int index = Array.IndexOf(numbers, search);
+
+            if (index != -1)
+            {
+                Console.WriteLine($"Number {search} found at Position {index + 1}");
+            }
+            else
+            {
+                Console.WriteLine($"Number {search} not found");
+            }
+
+            Console.WriteLine("Search the first half of the array");
+            Console.WriteLine("Enter a number to search: ");
+            search = Convert.ToInt32(Console.ReadLine());
+            index = Array.IndexOf(numbers, search, 0, numbers.Length / 2);
+
+            if (index != -1)
+            {
+                Console.WriteLine($"Number {search} found at Position {index + 1}");
+            }
+            else
+            {
+                Console.WriteLine($"Number {search} not found");
+            }
+
+            Console.WriteLine("Search the second half of the array");
+            Console.WriteLine("Enter a number to search: ");
+            search = Convert.ToInt32(Console.ReadLine());
+            index = Array.IndexOf(numbers, search, numbers.Length / 2, numbers.Length / 2);
+            if (index != -1)
+            {
+                Console.WriteLine($"Number {search} found at Position {index + 1}");
+            }
+            else
+            {
+                Console.WriteLine($"Number {search} not found");
             }
         }
     }
---
+++ FreeCodeCampCourse/ListsClass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListsClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            JustLists();
+            Console.ReadLine();
+        }
+
+        static void JustLists()
+        {
+            List<int> mArray = new List<int>()
+            {
+                5, 8, 10, 11, 3, 1, 5, 9, 2, 4, 25, 19, 6
+            };
+
+            Console.Write("Original: ");
+            for (int i = 0; i < mArray.Count; i++)
+            {
+                Console.Write($"{mArray[i]} ");
+            }
+
+            Console.WriteLine();
+            mArray.Sort();
+            Console.Write("Sorted: ");
+
+            foreach (int num in mArray)
+            {
+                Console.Write($"{num} ");
+            }
+        }
+    }
+}
